ffb4b6d3eb0082a6580b30487686ff4daa8db4f3
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-03-20 22:43:00 -0300

    Arquivos estáticos (staticfiles), STATIC_URL, STATICFILE_DIRS e load static

--- ola_django/project/settings.py
+++ ola_django/project/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'base'
+            BASE_DIR / 'base',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'base' / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
