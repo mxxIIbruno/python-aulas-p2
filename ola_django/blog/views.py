490b2787bd8a36152debec88845d8ae9ae18e223
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-03-21 21:22:05 -0300

    Configurando um post único no template post.html com a nova view e CSS

--- ola_django/blog/views.py
+++ ola_django/blog/views.py
@@ -1,45 +1,59 @@
+from typing import Any
 from django.shortcuts import render
+from django.http import HttpRequest
 from blog.data import posts
 
 
 def blog(request):
-    print('blog')
+    print("blog")
 
     context = {
         # 'text': 'Olá blog',
-        'title': 'BLOG -',
-        'posts': posts,
-    }
-
-    return render(request=request,
-                  template_name='blog/index.html',
-                  context=context)
-
-
-def post(request, id):
-    print('post', id)
-
-    context = {
-        # 'text': 'Olá blog',
-        'title': 'POST -',
-        'posts': posts,
-    }
-
-    return render(request=request,
-                  template_name='blog/index.html',
-                  context=context)
-
-
-def exemplo(request):
-    print('exemplo')
-
-    context = {
-        # 'text': 'Olá exemplo',
-        'title': 'EXEMPLO -'
+        "title": "BLOG -",
+        "posts": posts,
     }
 
     return render(
         request=request,
-        template_name='blog/exemplo.html',
+        template_name="blog/index.html",
+        context=context
+    )
+
+
+def post(request: HttpRequest, post_id: int):
+    found_post: dict[str, Any] | None = None
+
+    for post in posts:
+        if post["id"] == post_id:
+            found_post = post
+            break
+
+    if found_post is None:
+        raise Exception('Post não existe.')
+
+    context = {
+        # 'text': 'Olá blog',
+        "title": found_post['title'] + ' - ',
+        "post": found_post,
+    }
+
+    return render(
+        request=request,
+        template_name="blog/post.html",
+        context=context
+    )
+
+
+def exemplo(request):
+    print("exemplo")
+
+    context = {
+        # 'text': 'Olá exemplo',
+        "title": "EXEMPLO -"
+    }
+
+    return render(
+        request=request,
+        template_name="blog/exemplo.html",
         context=context
     )
